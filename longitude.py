726bba765b80b8e733bc272dad748043d78f67a5
Eric Harmon <eharmon@gmail.com>
2010-10-22 14:52:09 -0400

    Support arguments to make things more properly unix-y

--- longitude.py
+++ longitude.py
@@ -9,11 +9,19 @@ import datetime
 import json
 import sqlite3
 import ConfigParser
+import argparse
 
 import oauth2 as oauth
 
+parser = argparse.ArgumentParser(description='Fetch data from Google Latitude')
+parser.add_argument('--config', '-c', metavar='file', nargs='?', type=argparse.FileType('r'), default='longitude.cfg', dest='config_file', help='longitude configuration file')
+parser.add_argument('--keys', '-k',  metavar='file', nargs='?', type=argparse.FileType('r+'), default='keys', dest='keys_file', help='oauth key storage file')
+parser.add_argument('--db', '-d', metavar='file', nargs='?', type=str, default='loc_db', dest='db_file', help='sqlite storage DB for storing waypoints')
+parser.add_argument('-v', action='store_true', dest='verbose', help='be verbose')
+args = parser.parse_args()
+
 config = ConfigParser.RawConfigParser()
-config.read('longitude.cfg');
+config.readfp(args.config_file);
 
 scope = "https://www.googleapis.com/auth/latitude"
 loc = 'https://www.googleapis.com/latitude/v1/location?granularity=best&max-results=1000'
@@ -27,19 +35,19 @@ access_token_url = 'https://www.google.com/accounts/OAuthGetAccessToken'
 
 consumer = oauth.Consumer(domain, secret)
 
-if not os.path.isfile('keys'):
+oauth_token = args.keys_file.readline().strip()
+oauth_token_secret = args.keys_file.readline().strip()
+token = oauth.Token(oauth_token, oauth_token_secret)
+if(not oauth_token or not oauth_token_secret):
     client = oauth.Client(consumer)
+
+    print "Keys file is empty or corrupted, getting new authorization credentials..."
 
     # Get a request token.
     resp, content = client.request(request_token_url, "GET")
     if resp['status'] != '200':
         raise Exception("Invalid response %s." % resp['status'])
     request_token = dict(urlparse.parse_qsl(content))
-
-    print "Request Token:"
-    print "    - oauth_token        = %s" % request_token['oauth_token']
-    print "    - oauth_token_secret = %s" % request_token['oauth_token_secret']
-    print
 
     # Step 2: Link to web page where the user can approve the request token.
     print "Go to the following link in your browser:"
@@ -55,21 +63,13 @@ if not os.path.isfile('keys'):
     resp, content = client.request(access_token_url, "POST")
     access_token = dict(urlparse.parse_qsl(content))
 
-    print "Access Token:"
-    print "    - oauth_token        = %s" % access_token['oauth_token']
-    print "    - oauth_token_secret = %s" % access_token['oauth_token_secret']
-    print
-
     # Access content using access token
     token = oauth.Token(access_token['oauth_token'], access_token['oauth_token_secret'])
-    keys = open('keys', 'w')
-    keys.write(access_token['oauth_token'] + "\n")
-    keys.write(access_token['oauth_token_secret'])
-else:
-    keys = open('keys', 'r')
-    oauth_token = keys.readline().strip()
-    oauth_token_secret = keys.readline().strip()
-    token = oauth.Token(oauth_token, oauth_token_secret)
+    args.keys_file.write(access_token['oauth_token'] + "\n")
+    args.keys_file.write(access_token['oauth_token_secret'])
+
+    print "Authentication complete, starting initial import..."
+    args.verbose = True
 
 client = oauth.Client(consumer, token)
 
@@ -81,11 +81,15 @@ data = json.loads(content)
 
 items = data['data']['items']
 
-if(not os.path.isfile('track/loc_db')):
-    conn = sqlite3.connect('track/loc_db')
+if(not os.path.isfile(args.db_file)):
+    conn = sqlite3.connect(args.db_file)
     conn.execute('create table tracks_location (timestamp integer primary key, latitude real, longitude real, accuracy real)')
 else:
-    conn = sqlite3.connect('track/loc_db')
+    conn = sqlite3.connect(args.db_file)
+    try:
+        conn.execute('select * from tracks_location limit 1')
+    except sqlite3.OperationalError:
+        raise Exception('Unable to read from database, possible corruption?')
 
 c = conn.cursor()
 
@@ -103,4 +107,6 @@ for item in items:
 
 conn.commit()
 
-print "Added: " + str(new) + "\nSkipped: " + str(already) + "\n"
+if(args.verbose):
+    print "Import results:"
+    print "\tAdded: " + str(new) + "\n\tSkipped: " + str(already)
